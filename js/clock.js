7ea49e566ef77f926ce0b83051bdd8902340b72b
gatta.amin38@gmail.com <gatta.amin38@gmail.com>
2023-07-19 23:35:52 +0200

    add new icon + fix clock minutes + fix xmb_row_content css:white-space

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,7 @@ const clockSection = document.getElementById("clock");
 
 let setClock = () => {
     let d  = new Date()
-    let clock = `${d.getDate()}/${d.getMonth()+1} ${d.getHours()}:${d.getMinutes()}`
+    let clock = `${d.getDate()}/${d.getMonth()+1} ${d.getHours()}:${d.getMinutes().toString().padStart(2, '0')}`
     dateTime.innerText = clock
     setTimeout(setClock, 1000)
 };
