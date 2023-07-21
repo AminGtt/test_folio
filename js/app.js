631468c4f9a6824e1032042ea7958eb586990e3e
Amin Gatta <amin.gatta@arturia.com>
2023-07-21 15:42:15 +0200

    setup welcomer

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ const startupSound = document.getElementById("startup"),
 header = document.getElementById("header"),
 menu = document.getElementById("menu"),
 disclaimer = document.getElementById("disclaimer"),
-disclaimerTime = 500,
-headerTime = 500;
+welcomer = document.getElementById("welcomer"),
+disclaimerTime = 1000,
+headerTime = 1000;
 
 // startupSound.play();
 
@@ -12,6 +13,13 @@ let checkLoad = () =>{
         window.onload = resolve;
     });
 };
+
+let welcomerTimeOut = () =>{
+    return new Promise(resolve => {
+        setTimeout(resolve, 30000);
+    }
+    );
+}
 
 let headerTimeOut = () =>{
     return new Promise(resolve => {
@@ -37,6 +45,10 @@ let disclaimerDisplay = async () =>{
 
 let loadHeader = async () =>{
     await checkLoad();
+
+    await welcomerTimeOut()
+    welcomer.remove()
+
     header.style.opacity = '1';
     await disclaimerDisplay();
 };
@@ -44,7 +56,7 @@ let loadHeader = async () =>{
 let loadMenu = async () =>{
     await loadHeader();
     menu.style.opacity = '1';
-    setClock();
+    await setClock();
     clockSection.style.opacity = '1';
 };
 
